77d64d0e2d42b76e94b15e6cab5555db0057ce2f
Alexey Berezin <beraliv.spb@gmail.com>
2018-12-25 01:26:14 +0300

    prod: usePrevious & useEffect in ScrollToTop

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,16 +1,16 @@
-import { Component } from 'react'
+import { useEffect } from 'react'
 import { withRouter } from 'react-router'
+import usePrevious from '../util/usePrevious'
 
-class ScrollToTop extends Component {
-  componentDidUpdate (prevProps) {
-    if (this.props.location !== prevProps.location) {
+const ScrollToTop = ({ children = null, location }) => {
+  const previousLocation = usePrevious(location)
+  useEffect(() => {
+    if (location !== previousLocation) {
       window.scrollTo(0, 0)
     }
-  }
+  })
 
-  render () {
-    return this.props.children || null
-  }
+  return children
 }
 
 export default withRouter(ScrollToTop)
