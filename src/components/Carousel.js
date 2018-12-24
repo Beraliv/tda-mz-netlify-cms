cc9594c8f292e28a1cc8cbd1bc39e813a94d93c5
Alexey Berezin <beraliv.spb@gmail.com>
2018-12-25 00:57:26 +0300

    prod: useState in Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,44 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import SwipeableViews from 'react-swipeable-views'
 import Pagination from './Pagination'
 import LazyImage from './LazyImage'
 import './Carousel.css'
 
-export default class Carousel extends React.Component {
-  state = {
-    index: 0
-  }
+const Carousel = ({ title, images }) => {
+  const [index, setIndex] = useState(0)
 
-  handleIndexChange = index => this.setState({ index })
-
-  render () {
-    const { index } = this.state
-    const { title, images } = this.props
-
-    return (
-      <div className='container'>
-        <h1>{ title }</h1>
-        <SwipeableViews index={index} onChangeIndex={this.handleIndexChange}>
-          {images.map((image, index) => (
-            image && (
-              <LazyImage
-                key={`Carousel-${index}`}
-                className='Carousel--Image'
-                src={image}
-                alt='Carousel--Image'
-              />
-            )
-          ))}
-        </SwipeableViews>
-        {images && images.length > 1 && (
-          <Pagination
-            dots={images.length}
-            index={index}
-            onChangeIndex={this.handleIndexChange}
-          />
-        )}
-      </div>
-    )
-  }
+  return (
+    <div className='container'>
+      <h1>{ title }</h1>
+      <SwipeableViews index={index} onChangeIndex={setIndex}>
+        {images.map((image, index) => (
+          image && (
+            <LazyImage
+              key={`Carousel-${index}`}
+              className='Carousel--Image'
+              src={image}
+              alt='Carousel--Image'
+            />
+          )
+        ))}
+      </SwipeableViews>
+      {images && images.length > 1 && (
+        <Pagination
+          dots={images.length}
+          index={index}
+          onChangeIndex={setIndex}
+        />
+      )}
+    </div>
+  )
 }
+
+export default Carousel
